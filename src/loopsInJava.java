d35a9c749b8a2e49d69afa4fcfe045f254132321
Lachlan Chan <lcha691@aucklanduni.ac.nz>
2023-03-16 23:13:32 +1300

    Loops work

---
+++ src/loopsInJava.java
@@ -0,0 +1,18 @@
+public class loopsInJava {
+    
+    public static void main(String[]args){
+        // NOTE: Loops work exactly the same as they do in C
+
+        int a = 0;
+
+        for (int i = 0; i < 10; i++ ){
+            System.out.println("i is less than 10, i = " + i);
+        }
+
+        while (a<10){
+            System.out.println("a is less than 10, a = " + a);
+            a++;
+        }
+        
+    }
+}
