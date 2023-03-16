63c4a9bebf665b63e1153f83f3f15b87ca597f2a
Lachlan Chan <lcha691@aucklanduni.ac.nz>
2023-03-16 22:44:20 +1300

    strings practise

---
+++ src/Strings.java
@@ -0,0 +1,17 @@
+// Source file: Strings
+
+// Class: Strings
+public class Strings {
+
+        // method(similar to the function): main
+    public static void main(String[] args){
+        // practise printing on the command line
+        System.out.println("Madlibs!");
+        String username = "mariobro";
+        String adjective = "Super";
+        String verb = "jump";
+        String noun = "block";
+
+        System.out.println("My name is " + adjective + " " + username + " Wahoo" + ". I love to " + verb + " on " + noun + "!!");
+    }
+}
